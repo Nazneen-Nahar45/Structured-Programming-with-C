5918c9fb26b491ac2a8b16b49af744c91e67427c
Nazneen Nahar <nazneen.nahar145@gmail.com>
2022-12-04 21:39:07 +0600

    done

---
+++ Basic C/find_area_of_a_rectangle.c
@@ -0,0 +1,19 @@
+// find area of a rectangle
+
+#include <stdio.h>
+
+int main()
+{
+    float length, width, area;
+
+    printf("Enter length of rectangle: ");
+    scanf("%f", &length);
+    printf("Enter width of rectangle: ");
+    scanf("%d", &width);
+
+    area = length * width;
+
+    printf("Area of rectangle = %f sq. units ", area);
+
+    return 0;
+}
