9c75b44cbf9a6d0adce218b081eec8241eaf7ed0
Nazneen Nahar <nazneen.nahar145@gmail.com>
2023-01-03 23:22:53 +0600

    1

---
+++ functions and recursion/cube of any number..c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/* Function declaration */
+double cube(double num);
+
+int main()
+{
+    int num;
+    double c;
+    
+    /* Input number to find cube from user */
+    printf("Enter any number: ");
+    scanf("%d", &num);
+    
+    c = cube(num);
+
+    printf("Cube of %d is %.2f", num, c); 
+    
+    return 0;
+}
+
+/**
+ * Function to find cube of any number
+ */
+double cube(double num)
+{
+    return (num * num * num);
+}
